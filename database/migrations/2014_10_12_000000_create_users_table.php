30f8cadb7b6a6132a80b50dc9dabe9128d7aed9a
Amir Adibi <amir.adibinia@gmail.com>
2023-04-17 12:10:57 +0330

    feat: added 'StorePostRequest' form request in Modules/Admin/Http/Requests

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('level')->default('user');
+            $table->string('family')->nullable();
+            $table->string('mobile')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
